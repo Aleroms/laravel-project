fb3a675e67db1d705168cc92e6b54bcfaf694922
Santiago <santiagomorales333@yahoo.com>
2023-05-15 21:35:13 -0700

    learned about controllers, views, blade files and linking routes to controllers.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\http\Controllers\ExampleController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ExampleController::class,"homepage"]);
+
+Route::get('/about', [ExampleController::class,'aboutPage']);
