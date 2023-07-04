1c182e4279cd6ebd82eed5ef210da1d78fac3c94
Santiago <santiagomorales333@yahoo.com>
2023-07-04 16:41:57 -0700

    implemented homepage feed

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,4 +60,7 @@ class User extends Authenticatable
     public function followingTheseUsers(){
         return $this->hasMany(Follow::class,'user_id');
     }
+    public function feedPosts(){
+        return $this->hasManyThrough(Post::class,Follow::class,'user_id','user_id','id','followedUser');
+    }
 }
